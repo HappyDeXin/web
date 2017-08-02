d4b0ac64a8f3ce0d5cb73c5f328d0ea122edb36d
HappyDeXin <1492364487@qq.com>
2017-08-02 19:09:46 +0800

    建好的静态网页

---
+++ js/func/member/member.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    funcGetMember();
+});
+
+//获取类型
+function funcGetMember() {
+    $.ajax({
+        type: "get",
+        dataType: "json",
+        url: 'http://123.57.26.127/api/Member/GetMemberList/?pageIndex=1&pageSize=20',
+        data: {},
+        success: function (data) {
+            funcInitMember(data);
+        }, error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("未能获取友情链接类型");
+        }
+    });
+}
+
+//初始化会员
+function funcInitMember(member) {
+    var ulLeft = $('#ulLeft');
+    var ulRight = $('#ulRight');
+	var homepage_members = $('#homepage_members');
+    
+    for (var i = 0; i < member.resultList.length; i++) {
+        if (i % 2 == 0) {
+            ulLeft.append('<li><a href="' + member.resultList[i].mURL + '" target="_blank">' + member.resultList[i].mName + '<span >' + member.resultList[i].mApplyTime + '</span></a></li>');
+        } else {
+            ulRight.append('<li><a href="' + member.resultList[i].mURL + '" target="_blank">' + member.resultList[i].mName + '<span >' + member.resultList[i].mApplyTime + '</span></a></li>');
+        }
+		//首页会员单位
+	    homepage_members.append('<li><a href="' + member.resultList[i].mURL + '" target="_blank">' + member.resultList[i].mName + '</span></a></li>');
+    }
+	
+}
